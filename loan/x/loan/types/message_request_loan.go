89efcacd6f59e1ecd078a444533df2a3d816570d
QuePark <gyuhapark11@gmail.com>
2023-04-14 17:23:12 +0900

    DeFi Loan Practice: Request a loan

--- loan/x/loan/types/message_request_loan.go
+++ loan/x/loan/types/message_request_loan.go
@@ -1,6 +1,8 @@
 package types
 
 import (
+	"strconv"
+
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 )
@@ -44,6 +46,34 @@ func (msg *MsgRequestLoan) ValidateBasic() error {
 	_, err := sdk.AccAddressFromBech32(msg.Creator)
 	if err != nil {
 		return sdkerrors.Wrapf(sdkerrors.ErrInvalidAddress, "invalid creator address (%s)", err)
+	}
+	amount, _ := sdk.ParseCoinsNormalized(msg.Amount)
+	if amount.IsValid() == false {
+		return sdkerrors.Wrap(sdkerrors.ErrInvalidRequest, "amount is not a valid Coins object %s")
+	}
+	if amount.Empty() {
+		return sdkerrors.Wrap(sdkerrors.ErrInvalidRequest, "amount is empty")
+	}
+
+	fee, _ := sdk.ParseCoinsNormalized(msg.Fee)
+	if fee.IsValid() == false {
+		return sdkerrors.Wrap(sdkerrors.ErrInvalidRequest, "fee is not a valid Coins object")
+	}
+
+	deadline, err := strconv.ParseInt(msg.Deadline, 10, 64)
+	if err != nil {
+		return sdkerrors.Wrap(sdkerrors.ErrInvalidRequest, "deadline is not a integer")
+	}
+	if deadline <= 0 {
+		return sdkerrors.Wrap(sdkerrors.ErrInvalidRequest, "deadline should be a positive integer")
+	}
+
+	collateral, _ := sdk.ParseCoinsNormalized(msg.Collateral)
+	if collateral.IsValid() == false {
+		return sdkerrors.Wrap(sdkerrors.ErrInvalidRequest, "collateral is not a valid Coins object")
+	}
+	if collateral.Empty() {
+		return sdkerrors.Wrap(sdkerrors.ErrInvalidRequest, "collateral is empty")
 	}
 	return nil
 }
