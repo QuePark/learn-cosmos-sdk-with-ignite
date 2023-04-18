c67a653cabe73012fdd1dc6ef4bd7b9820bb01e6
QuePark <gyuhapark11@gmail.com>
2023-04-18 13:39:14 +0900

    Interchange Practice: Add Mint and Burn Voucher

---
+++ interchange/x/dex/keeper/denom.go
@@ -0,0 +1,43 @@
+package keeper
+
+import (
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	ibctransfertypes "github.com/cosmos/ibc-go/v6/modules/apps/transfer/types"
+
+	"interchange/x/dex/types"
+)
+
+func (k Keeper) SaveVoucherDenom(ctx sdk.Context, port string, channel string, denom string) {
+	voucher := VoucherDenom(port, channel, denom)
+
+	_, saved := k.GetDenomTrace(ctx, voucher)
+	if saved == false {
+		k.SetDenomTrace(ctx, types.DenomTrace{
+			Index:   voucher,
+			Port:    port,
+			Channel: channel,
+			Origin:  denom,
+		})
+	}
+}
+
+func VoucherDenom(port string, channel string, denom string) string {
+	sourcePrefix := ibctransfertypes.GetDenomPrefix(port, channel)
+
+	prefixedDenom := sourcePrefix + denom
+
+	denomTrace := ibctransfertypes.ParseDenomTrace(prefixedDenom)
+	voucher := denomTrace.IBCDenom()
+	return voucher[:16]
+}
+
+func (k Keeper) OriginalDenom(ctx sdk.Context, port string, channel string, voucher string) (string, bool) {
+	trace, exist := k.GetDenomTrace(ctx, voucher)
+	if exist == true {
+		if trace.Port == port && trace.Channel == channel {
+			return trace.Origin, true
+		}
+	}
+
+	return "", false
+}
