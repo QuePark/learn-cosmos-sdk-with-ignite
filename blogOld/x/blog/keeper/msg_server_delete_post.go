344e3aa652660131c5d9d98aeb00abc160bbbf7a
QuePark <gyuhapark11@gmail.com>
2023-04-19 11:09:27 +0900

    Client Practice: renamed blog to blogOld

---
+++ blogOld/x/blog/keeper/msg_server_delete_post.go
@@ -0,0 +1,28 @@
+package keeper
+
+import (
+	"context"
+	"fmt"
+
+	"blog/x/blog/types"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
+)
+
+func (k msgServer) DeletePost(goCtx context.Context, msg *types.MsgDeletePost) (*types.MsgDeletePostResponse, error) {
+	ctx := sdk.UnwrapSDKContext(goCtx)
+
+	val, found := k.GetPost(ctx, msg.Id)
+	if found == false {
+		return nil, sdkerrors.Wrap(sdkerrors.ErrKeyNotFound, fmt.Sprintf("key %d doesn't exist", msg.Id))
+	}
+
+	if msg.Creator != val.Creator {
+		return nil, sdkerrors.Wrap(sdkerrors.ErrUnauthorized, "incorrect owner")
+	}
+
+	k.RemovePost(ctx, msg.Id)
+
+	return &types.MsgDeletePostResponse{}, nil
+}
