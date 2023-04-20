1bd52206f2708a7f822c11db99922151320db478
QuePark <gyuhapark11@gmail.com>
2023-04-20 18:11:32 +0900

    TypeScript Frontend Practice: Try to connect remote testnet node. but there is an error CORS

--- example/my-frontend-app/src/main.ts
+++ example/my-frontend-app/src/main.ts
@@ -11,79 +11,23 @@ import { Client } from "../../ts-client/";
 // const [{ address }] = await wallet.getAccounts();
 
 const client = new Client({
-	apiURL: "http://localhost:1317",
-	rpcURL: "http://localhost:26657",
-	prefix: "cosmos",
+	apiURL: "http://seed-1.mainnet.rizon.world:1317",
+	rpcURL: "http://seed-1.mainnet.rizon.world:26657",
+	prefix: "rizon",
 });
 
-// const txResult = await client.CosmosBankV1Beta1.tx.sendMsgSend({
-// 	value: {
-// 		amount: [
-// 			{
-// 				amount: "200",
-// 				denom: "token",
-// 			},
-// 		],
-// 		fromAddress: address,
-// 		toAddress: "cosmos15uw6qpxqs6zqh0zp3ty2ac29cvnnzd3qwjntnc",
-// 	},
-// 	fee: {
-// 		amount: [{ amount: "0", denom: "stake" }],
-// 		gas: "200000",
-// 	},
-// 	memo: "Hello world",
-// });
-
-// console.log("txResult", txResult);
-
-// const balances = await client.CosmosBankV1Beta1.query.queryAllBalances("cosmos13xkhcx2dquhqdml0k37sr7yndquwteuvt2cml7");
-// console.log(balances.data.balances);
-
-await client.useKeplr().then(async () => {
-	const { keplr } = client.signer;
-	console.log("keplr", keplr);
-	const chainInfos = await keplr.getChainInfosWithoutEndpoints();
-	console.log(chainInfos);
-	if (chainInfos.filter((x) => x.chainId === "groot-16").length === 0) {
-		const obj = Object.assign({}, chainInfos[0]);
-		const groot16 = {
-			...obj,
-			chainId: "groot-16",
-			chainName: "RIZON TestNet",
-			currencies: [
-				{
-					coinDenom: "ATOLO",
-					coinMinimalDenom: "uatolo",
-					coinDecimals: 6,
-					coinGeckoId: "rizon",
-				},
-			],
-			feeCurrencies: [
-				{
-					coinDenom: "ATOLO",
-					coinMinimalDenom: "uatolo",
-					coinDecimals: 6,
-					coinGeckoId: "rizon",
-					gasPriceStep: {
-						low: 0.0025,
-						average: 0.025,
-						high: 0.04,
-					},
-				},
-			],
-			features: [
-				"stargate",
-				"ibc-transfer",
-				"no-legacy-stdTx",
-				"no-legacy-stdTx",
-				"ibc-go",
-				"query:/cosmos/bank/v1beta1/spendable_balances",
-			],
-			rpc: "http://seed-1.mainnet.rizon.world:26657/",
-		};
-		keplr.experimentalSuggestChain(groot16);
-	}
+await client.useKeplr({
+	chainName: "Rizon Testnet",
+	stakeCurrency: {
+		coinDenom: "AOTOLO",
+		coinMinimalDenom: "uatolo",
+		coinDecimals: 6,
+	},
 });
+
+const account = await client.CosmosAuthV1Beta1.query.queryAccounts();
+
+console.log(account);
 
 document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
   <div>
